c1a854dff3357398cfc29a6744c94fb424d4efc2
Bento da Silva Junior <osmarb@38f9d361fe56.ant.amazon.com>
2020-11-03 18:06:15 -0300

    master

---
+++ amplify/backend/function/cfjwtauth/src/index.js
@@ -0,0 +1,14 @@
+
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './img/aws.png';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import player from './player';
+import testplayer from './testplayer';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import awsmobile from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsmobile);
+
+class App extends Component {
+
+  render() {
+    return (
+    <Router>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <ul className="navbar-nav mr-auto">
+          <a class="navbar-brand" href="#">
+            <img src={logo} alt={logo} width="65"/>
+          </a>
+            <li><Link to={'/'} className="nav-link">TestPlayer</Link></li>
+          </ul>
+             <ul id="nav-mobile" className="right navbar-nav">
+            <li className="float-right">
+                  <a className="nav-link float-right" href="/" >Logout</a>
+            </li>
+          </ul>
+          </nav>
+          <hr />
+
+
+
+
+          <Switch>
+              <Route exact path='/player' component={player} />
+              <Route exact path='/' component={testplayer} />
+          </Switch>
+
+          <div>
+           </div>
+
+        </div>
+      </Router>
+
+
+
+    );
+  }
 }
+    
 
-export default App;
+export default withAuthenticator(App);
