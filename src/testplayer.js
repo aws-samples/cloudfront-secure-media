69d2793dca10212863255a633f07e217648d51c5
Bento da Silva Junior <osmarb@38f9d361fe56.ant.amazon.com>
2020-12-08 10:38:20 -0300

    version 0.1.6

--- src/testplayer.js
+++ src/testplayer.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react';
-//import getToken from './gettoken';
-//import './App.css';
 import VideoPlayer from './player';
 import Amplify, { Auth } from 'aws-amplify';
 import awsmobile from "./aws-exports";
-import jwt  from 'jsonwebtoken';
 import Table from 'react-bootstrap/Table';
-
-
-
 
 Amplify.configure(awsmobile);
 
-
-
-
 class testplayer extends Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
@@ -25,47 +14,23 @@ class testplayer extends Component {
       videoURL: "https://dtwq7j2a2lfi4.cloudfront.net/staticVideos/Big_Buck_Bunny_720_10s_5MB.mp4",
       showComponent: true,
     };
-    //updateState = updateState.bind(this)
     this.handleURLset = this.handleURLset.bind(this);
   }
 
   componentDidMount() {
-
-    //this.interval = setInterval(() => this.tick(), 1000000000);
-   
-    //this.getIVSToken()
     this.getCurrentUser()
-    //console.log("time", this.interval);
   }
- 
- 
+
   componentWillUnmount() {
-    //clearInterval(this.interval);
   }
- 
 
 getCurrentUser() {
   Auth.currentSession({ bypassCache: true }).then(session => {
     this.setState({
       session,
-      //username: user.username,
       token: session.accessToken.jwtToken
-
     });
-    console.log("User ALL", session);
-    //console.log("Username", this.state.username);
-    console.log("Access Token", this.state.token);
-    var jwtToken = this.state.token;
-    var decodedJwt = jwt.decode(jwtToken, {complete: true});
-    console.log("Decoded Token", decodedJwt);
   });
-};
-
-getPlayerState(){
-  console.log("player state", this);
-  console.log("PROPS!", this.props);
-  console.log("gett!!!!", this.playerState);
-  //this.props.sendFunction();
 };
 
 handleURLset = (e) =>{
@@ -76,7 +41,6 @@ handleURLset = (e) =>{
     videoURL,
     showComponent: true
   })
-  console.log("Player", this.state)
   this.playerShow()
 } 
 
@@ -88,81 +52,23 @@ playerShow = () => {
       width: 720,
       height: 420,
       bigPlayButton: true,
-      //token: this.getIVSToken(),
-      //html5: html5Settings,
       token: this.state.token,
       cookie: "testOsmar",
       sources: [{
         src: this.state.videoURL,
-        type: 'video/mp4',
+        type: 'application/x-mpegURL',
       }]
     }}/>}</div>
   );
 }
-  
-
 
   render() {
-
     console.log("reder has been called");
     console.log(this.state);
     document.body.style = 'background: #262626;';
     const { token, showComponent } = this.state;
     console.log("tem token?", token)
     const { videoURL }  = this.state;
-
-  
-
-    const {videoJsOptions} = this.state;
-    
-
-    
-
-
-    
-    
-    const videoJsOptionsB = {
-      autoplay: false,
-      controls: true,
-      width: 720,
-      height: 420,
-      bigPlayButton: true,
-      //token: this.getIVSToken(),
-      //html5: html5Settings,
-      token,
-      cookie: "testOsmar",
-      sources: [{
-        src: `https://dc17l10byq54i.cloudfront.net/out/v1/98bd52858aac4fc79ad082122402d9e0/clear.m3u8?token=`,
-        type: 'application/x-mpegURL',
-      }]
-    }
-
-    const videoJsOptionsC = {
-      autoplay: true,
-      controls: true,
-      width: 720,
-      height: 420,
-      bigPlayButton: false,
-      //token: this.getIVSToken(),
-      //html5: html5Settings,
-      token,
-      cookie: "testOsmar",
-      headerplayer: "{cogtoken}",
-      /*headers: {
-        "Content-Type": "application/json",
-        "ThisIsa-test" : "minhola"
-      },*/
-      sources: [{
-        src: `https://d1c9o7kub5969p.cloudfront.net/output/HLS/video.m3u8`,
-        type: 'application/x-mpegURL',
-      }],
-
-      options: [{ 
-        headers: { Authorization: `${token}`}
-      }] 
-    }
-
-    
     if (token === 0) {
       console.log("loadding");
       return (
@@ -180,11 +86,8 @@ playerShow = () => {
             <div className="row">
             <div className="col-sm-1"></div>
             <div className="col-lg">
-
-            
             <div className="form-group">
               <form className="form-URL">
-              
                 <label className="formLabel">
                   Video URL (.m3u8):
                       <input 
@@ -199,34 +102,22 @@ playerShow = () => {
                         />
                       </label>
                     <button type="submit" className="formBot" onClick={this.handleURLset}>Play</button>
-                
                 </form>
                 </div>
               </div>
             </div>
-
-      
-               
-            
-         
-
-
             <div className="player-wrapper">
-          
             {showComponent && (
               <div>
               {this.playerShow()}
               </div>
             )}
             </div>
-
             <div className="DebugBOXger">
               <div className="DebugBOXtitle">
                 <a>Info:</a>
               </div>
               <div className="DebugBOX">
-
-
               <Table className="DebugTable" variant="dark" responsive="lg" >
                   <tbody wordBreak='break-all'>
                     <tr>
@@ -245,22 +136,13 @@ playerShow = () => {
                     </tr>
                   </tbody>
               </Table>
-
-
-
               </div>
-            
-    
-  
-            </div>
-
-          
+            </div>         
             </div>
         </div>
       );
     }
   }
 }
-
 export default testplayer;
 
