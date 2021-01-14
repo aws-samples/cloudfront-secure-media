a4ff93c1ef3c4ba7197f5ef0b0c53f99d83e91f9
Osmar Junior <osmarb@amazon.com>
2021-01-14 19:08:22 -0300

    config file

--- amplify/backend/function/jwtauth/src/config.js
+++ amplify/backend/function/jwtauth/src/config.js
@@ -2,5 +2,5 @@ var config = {};
 
 config.REGION = process.env.AWS_REGION
 config.USERPOOLID = '' // please add you cognito user pool id, you can run amplify auth console and select 'User Pool option'
-config.JWKS = ''
+config.JWKS = '' // please add the cognito well-known JWKS, please read step 5 
 module.exports = config;
